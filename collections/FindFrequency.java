f532c33eccbacca65d75260062b908227c83a82d
amanagg639 <amanagg639@gmail.com>
2025-03-21 10:06:00 +0530

    [aman] Add. find frequency  of elements using hashmap

---
+++ collections/FindFrequency.java
@@ -0,0 +1,21 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FindFrequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int len = scanner.nextInt();
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=0; i<len; i++){
+            list.add(scanner.next());
+        }
+        HashMap<String, Integer>map = new HashMap<>();
+        for(String s : list){
+            map.put(s, map.getOrDefault(s, 0)+1);
+        }
+        System.out.println(map);
+    }
+}
