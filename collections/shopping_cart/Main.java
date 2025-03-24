f964043f5275471d603d863273f0b1d975fd594b
amanagg639 <amanagg639@gmail.com>
2025-03-24 23:48:04 +0530

    [Aman] Add. shopping cart

---
+++ collections/shopping_cart/Main.java
@@ -0,0 +1,19 @@
+package collections.shopping_cart;
+
+public class Main {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        Product product1 = new Product("Apple", 199);
+        Product product2 = new Product("Banana", 399);
+        Product product3 = new Product("Orange", 99);
+
+        cart.addProduct(product1);
+        cart.addProduct(product2);
+        cart.addProduct(product3);
+        cart.addProduct(product1);
+
+        cart.display();
+    }
+}
+
