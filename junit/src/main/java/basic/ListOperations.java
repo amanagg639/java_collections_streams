6e5a64ab2c910af7bcc8e4e40b8300935be87c6f
amanagg639 <amanagg639@gmail.com>
2025-03-31 10:05:10 +0530

    [Aman] Add. adding list operations main code

---
+++ junit/src/main/java/basic/ListOperations.java
@@ -0,0 +1,15 @@
+package basic;
+
+import java.util.List;
+
+public class ListOperations {
+    public static void addElement(List<Integer> list, int element){
+        list.add(element);
+    }
+    public static void removeElement(List<Integer> list, int element){
+          list.remove(Integer.valueOf(element));
+    }
+    public static int getSize(List<Integer> list){
+        return list.size();
+    }
+}
