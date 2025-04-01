edd65cbdf44a57214788ee63d45b58338f46af56
amanagg639 <amanagg639@gmail.com>
2025-04-01 14:09:21 +0530

    [Aman] Add. suppress warning annotation

---
+++ reflection_annotation/annotation/SuppressWarnings.java
@@ -0,0 +1,11 @@
+package reflection_annotation.annotation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuppressWarnings {
+    public static void main(String[] args) {
+//        @SuppressWarnings("unchecked")
+//        List<String> list =new ArrayList<>();
+    }
+}
